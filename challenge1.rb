9207dd86f197f260125e622b2dcb67611507d45b
Dewey <admin@failverse.com>
2013-03-17 23:58:28 +0000

    Commit for Challenge 1

---
+++ challenge1.rb
@@ -0,0 +1,54 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'fog'
+
+
+#You can override the ~/.rackspace_cloud_credentials here if you want. 
+username = ""
+api_key = ""
+
+
+#Set up the vars for username and api_key
+if File.exists?(File.join(Dir.home, ".rackspace_cloud_credentials")) && username.empty? && api_key.empty?
+	f = File.open(File.join(Dir.home, ".rackspace_cloud_credentials"), 'r')
+
+	username = f.gets.split(':')[1].strip
+	api_key = f.gets.split(':')[1].strip
+	
+	f.close
+elsif ! username.empty? && ! api_key.empty?
+	#This is only here so we don't raise an exception if the credentials file isn't there while we're using overrides
+else
+	raise "~/.rackspace_cloud_credentials not found"
+end
+
+
+
+#Set up our Compute object.
+conn = Fog::Compute.new(
+	:provider => 'Rackspace',
+	:rackspace_api_key => api_key,
+	:rackspace_username => username,
+	:version => :v2
+)
+
+
+[1,2,3].each do |var|
+	server = conn.servers.create(:flavor_id => 2, :image_id => 'a10eacf7-ac15-4225-b533-5744f1fe47c1', :name => "web#{var}")
+	puts "Starting Build..."
+	server.reload
+	until ! server.public_ip_address.empty? 
+		#Block until We have an IP 
+		puts "Build Started, waiting on IP..." 
+		server.reload
+		sleep 5 
+	end
+#	server.wait_for { ready? }
+	puts "==========Web#{var} Information========="
+	puts "Public IP Address: #{server.public_ip_address}"
+	puts "Password: #{server.password}"
+end
+
+
+
