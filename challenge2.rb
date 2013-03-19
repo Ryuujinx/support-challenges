f2806995ef309218d1b42c31cd3d49c8aadd4b8d
Dewey <admin@failverse.com>
2013-03-19 04:15:02 +0000

    New Challenge Upload

---
+++ challenge2.rb
@@ -0,0 +1,107 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'fog'
+
+
+
+#This  will read from ~/.rackspace_cloud_credentials
+#The format should be
+#username: <User>
+#api_key: <Api_key>
+
+
+
+#Make sure we pass in a servername/uuid
+unless ARGV.size > 0
+	puts "usage: #{$0} servername|uuid"
+	exit(1)
+end
+
+#Save ARGV[1] so we can manipulate the variable later. We could only accept UUIDs, but this allows more flexibility
+
+servname = ARGV[0]
+
+
+
+#You can override the ~/.rackspace_cloud_credentials here if you want. 
+username = ""
+api_key = ""
+
+
+#Set up the vars for username and api_key
+if File.exists?(File.join(Dir.home, ".rackspace_cloud_credentials")) && username.empty? && api_key.empty?
+	f = File.open(File.join(Dir.home, ".rackspace_cloud_credentials"), 'r')
+
+	username = f.gets.split(':')[1].strip
+	api_key = f.gets.split(':')[1].strip
+	
+	f.close
+elsif ! username.empty? && ! api_key.empty?
+	#This is only here so we don't raise an exception if the credentials file isn't there while we're using overrides
+else
+	raise "~/.rackspace_cloud_credentials not found"
+end
+
+
+
+#Set up our Compute object.
+conn = Fog::Compute.new(
+	:provider => 'Rackspace',
+	:rackspace_api_key => api_key,
+	:rackspace_username => username,
+	:version => :v2
+)
+
+
+
+
+#Some Logic to try and figure out if ARGV[1] is a UUID or a Server.
+begin
+	# First try it as a UUID
+	server = conn.servers.get(servname)
+	if server.nil?
+		raise Fog::Compute::RackspaceV2::NotFound  #Apparently whoever wrote Fog decided it would be cute to return Nil instead of Fog::Compute::RackspaceV2::NotFound, as such we raise it ourselves here. I feel this is a bit cleaner then redefining the method. 
+	end
+rescue Fog::Compute::RackspaceV2::NotFound
+	unless servname != ARGV[0] #We only want to do the lookup once
+		conn.servers.all.each do |servobj|
+			if servobj.name == ARGV[0]
+				servname = servobj.id
+				break  #No reason to keep going, we have our server. 
+			end
+		servname = nil
+		end
+	retry
+	end
+	
+end
+
+
+#Now we make an image, yay
+
+image = server.create_image("#{server.name}-Clone-#{Time.new.strftime("%Y-%m-%d")}")
+puts "Started Creating Image..."
+
+
+until image.ready?
+	#Block Until the image is created
+	puts "Waiting on image to complete, this step can take a while..."
+	sleep 10
+	image.reload
+end
+
+
+
+newserv = conn.servers.create(:flavor_id => server.flavor, :image_id => image.id, :name => "#{server.name}-clone-#{Time.new.strftime("%Y-%m-%d")}")
+puts "Starting Build..."	
+until ! newserv.public_ip_address.empty?
+	#Block until We have an IP
+	puts "Build Started, waiting on IP..."
+	newserv.reload
+	sleep 5
+end
+
+puts "==========#{server.name}-clone-#{Time.new.strftime("%Y-%m-%d")} Information========="
+puts "Public IP Address: #{newserv.public_ip_address}"
+puts "Password: #{newserv.password}"
